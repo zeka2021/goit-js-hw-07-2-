a95c4a448b6ff34103f946641d969ba6a77c6e5f
zeka2021 <gerasimovajeka@gmail.com>
2021-07-03 23:06:42 +0300

    correct task-06.js 07.js

--- js/task-06.js
+++ js/task-06.js
@@ -9,19 +9,51 @@
 //     }
 // };
 
+//  const inputRef = document.querySelector('#validation-input');
+// inputRef.addEventListener('focus', focusInputRef)
+// function focusInputRef(event) {
+//     const inputLength = Number(event.target.dataset.length);
+//     const inputValue = event.target.value.length;
+//     if (inputLength === inputValue) {
+//         inputRef.classList.add('valid');
+//         inputRef.classList.remove('invalid');
+//     } else {
+//         inputRef.classList.remove('valid');
+//         inputRef.classList.add('invalid');
+//     }
+//     console.log(inputLength)
+//     console.log(inputValue)
+//     console.log(inputLength === inputValue)
+// }
+// const getInput = document.querySelector('validation-input');
+// const getDataLength = Number(getInput.getAttribute('data-length'));
+// const inputIsValid = getInput.addEventListener('focus', onFormInput);
+// function onFormInput(event) {
+//     const getInputValue = event.currentTarget.value;
+
+//     if (getInputValue.length === getDataLength) {
+//         getInput.classList.add('valid');
+//         getInput.classList.remove('invalid');
+//     }
+//     else {
+//         getInput.classList.remove('valid');
+//         getInput.classList.add('invalid');
+//     }
+// };
+
+
+
 const inputRef = document.querySelector('#validation-input');
-inputRef.addEventListener('focus', focusInputRef)
 function focusInputRef(event) {
-    const inputLength = Number(event.target.dataset.length);
-    const inputValue = event.target.value.length;
-    if (inputLength === inputValue) {
-        inputRef.classList.add('valid');
-        inputRef.classList.remove('invalid');
-    } else {
-        inputRef.classList.remove('valid');
-        inputRef.classList.add('invalid');
-    }
-    console.log(inputLength)
-    console.log(inputValue)
-    console.log(inputLength === inputValue)
+  const isValidLength =
+    event.target.value.length === Number(event.target.dataset.length);
+  if (isValidLength) {
+    inputRef.classList.add('valid');
+    inputRef.classList.remove('invalid');
+  } else {
+    inputRef.classList.remove('valid');
+    inputRef.classList.add('invalid');
+  }
 }
+inputRef.addEventListener('blur', focusInputRef);
+console.log(inputRef);
--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,18 @@
-const controlFont = document.getElementById("font-size-control");
+// const controlFont = document.getElementById("font-size-control");
 
-const text = document.getElementById("text");
+// const text = document.getElementById("text");
 
-controlFont.oninput = function() {
-  text.style.fontSize = controlFont.value + "px";
-};
+// controlFont.oninput = function() {
+//   text.style.fontSize = controlFont.value + "px";
+// };
+const controlFont = document.getElementById('font-size-control');
+const text = document.getElementById('text');
+const inputValue = controlFont.value;
+function changeFontSize(inputValue) {
+  text.style.fontSize = `${inputValue}px`;
+}
+function oninput(event) {
+  changeFontSize(event.target.value);
+}
+controlFont.addEventListener('input', oninput);
+changeFontSize(inputValue);
